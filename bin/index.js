bfcf00a1ed6b04157ceccfb7883f0671f869270f
David White-Goode <dwhitegoode@gmail.com>
2022-10-25 13:36:56 -0400

    output

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+
+fs.readFile(__dirname + '/output', 'utf8', function (err, data) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+    return data
+  }
+})
+
